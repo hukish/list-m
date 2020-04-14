f42da1d7ec8bfbb2f3063bd78476c50652363518
hukish <hudsonhukish@gmail.com>
2020-04-14 20:29:36 +0300

    commenit

---
+++ start.sh
@@ -0,0 +1,6 @@
+export MOVIE_API_KEY="0d791f9e8e18a001bb3c2d26714f2745"
+export SECRET_KEY='1234'
+export MAIL_USERNAME=hudsonhukish@gmail.com
+export MAIL_PASSWORD=kishoyian1234
+
+python3.6 manage.py server
